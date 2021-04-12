267c30d0313ae3a54353c0c16da81eacea47cec3
iikim <iikim2511@gmail.com>
2021-04-12 16:38:47 -0400

    fix: run again when hit answer

--- script.js
+++ script.js
@@ -78,16 +78,7 @@ function run(){
  
     wordDisplay.innerHTML = words[Math.floor(Math.random()*100)];
 
-/* 
-    const randomIndex = Math.floor(Math.random() * words.length)
-    wordDisplay.innerText = words[randomIndex];
-    runNotification('success')
- */
-    
-
-    // wordInput.addEventListener('change',match);   
 }
-
 
 // js 2-1. input enter key (same coding with "js 2")
 wordInput.addEventListener('keypress', run_enter);
@@ -98,19 +89,15 @@ function run_enter(e) {
       }    
 }
 
-
-
 // js 2-2. countDown
 function countDown() {    
     if (time > 0) {
         time--;        
     }else if(time === 0){
         // wordDisplay.innerHTML = words[Math.floor(Math.random()*100)];
-        run();
-       
+        run();       
     }    
-    timeDisplay.innerHTML = time;
-    
+    timeDisplay.innerHTML = time;    
 }
 
  // js 2-2. count down, setInterval 
@@ -122,23 +109,21 @@ function countDown() {
 //  timeInterval = setInterval(countDown, 1000);
  // clearInterval(timeInterval);
 
-
-
-
 // js 2-3. buttonChange
 function buttonChange(p_text) {
     button.innerHTML=p_text;    
 }
 
-
 // js 2-4 input.value = wordDisplay.inner html , check
 function match(){
 
-    if (wordDisplay.innerHTML.toLowerCase() === wordInput.value.toLowerCase()) {            //🐞 ??bug
+    if (wordDisplay.innerHTML.toLowerCase() === wordInput.value.toLowerCase()) { 
         score++
         scoreDisplay.innerHTML = score;        
         wordInput.value = "";
         wordDisplay.innerHTML = words[Math.floor(Math.random()*100)];
+        run();
+        
     }   
 }
 
@@ -151,15 +136,11 @@ function matchWrong(){
     } 
 }
 
-
-
 /* js 10. button_stop 
 
 1. change button to game start
 2.  classlist remove of button
-3. reset time
-4. reset new word                                                          🌊  
-
+3. reset time and stop                                                  🌊  
 */
 
 buttonStop.addEventListener('click',stop);
@@ -168,6 +149,6 @@ function stop() {
     buttonChange('Game Start')
     button.classList.remove('loading');
 
-    time = gameTime;
-    timeDisplay=time;
+    time = 0;
+    timeDisplay.innerHTML = "0";
 }
