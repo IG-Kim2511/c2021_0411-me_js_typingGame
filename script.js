7b2afc6ecd3f4ad63b6c842f11faefba419159b8
iikim <iikim2511@gmail.com>
2021-04-11 23:05:50 -0400

    js 4. button_stop   1. change button to game start 2.  classlist remove */
     js 4. button_stop
    
    1. change button to game start
    2.  classlist remove */

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const button = document.querySelector('.button');
 
 const timeDisplay = document.querySelector('.time');
 
-
+// js 2. button
 button.addEventListener('click', run);
 function run(){
     /* 1. 버튼이 게임중으로 표시
@@ -19,7 +19,7 @@ function run(){
         2-1 . 맞추면 스코어 , 틀리면 다음단어로
     3 . new word show
     4. 단어 맞추면 스코어 */
-    // buttonChange('gaming...');
+    buttonChange('gaming...');
     button.classList.add('loading');
 
     countDown()
@@ -40,3 +40,22 @@ function countDown() {
 
 //     button.innertext(a_text);    
 // }
+
+function buttonChange(p_text) {
+    button.innerHTML=p_text;
+    
+}
+
+/* js 4. button_stop 
+
+1. change button to game start
+2.  classlist remove */
+
+const buttonStop= document.querySelector('.button_stop');
+
+buttonStop.addEventListener('click',stop);
+
+function stop() {
+    buttonChange('Game Start')
+    button.classList.remove('loading');
+}
