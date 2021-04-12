0a0b051fc81fcca2314c18c08f4fb2be0f003867
iikim <iikim2511@gmail.com>
2021-04-12 11:24:39 -0400

    random word display on input

--- script.js
+++ script.js
@@ -4,17 +4,18 @@
 3 . new word show
 4. 단어 맞추면 스코어 */
 
-
-let time = 5;
+let gameTime = 3;
+let time = gameTime;
 let timeInterval;
 let score = 0;
+let words= []
 
+
+const url = "https://random-word-api.herokuapp.com/word?number=100";
 const button = document.querySelector('.button');
 const buttonStop= document.querySelector('.button_stop');
 
 const timeDisplay = document.querySelector('.time');
-
-
 const wordDisplay = document.querySelector('.word-display');
 const wordInput = document.querySelector('.word-input');
 const scoreDisplay = document.querySelector('.score');
@@ -27,13 +28,15 @@ function run(){
     /* 1. 버튼이 게임중으로 표시
     2. 카운트 다운
         2-1 . 맞추면 스코어 , 틀리면 다음단어로
-    3 . new word show
-    4. 단어 맞추면 스코어, input value to empty */
+    3 . new word show                                                          🌊  
+    4. 단어 맞추면 스코어, input value to empty                                                           🌊  
+    */
     buttonChange('gaming...');
     button.classList.add('loading');
 
     countDown()
-
+    
+   
     console.log(wordDisplay.innerHTML)
     console.log(wordInput.value)
 
@@ -44,6 +47,22 @@ function run(){
         scoreDisplay.innerHTML = score;        
         wordInput.value = "";
     }
+
+    
+    getWords();           //js 4. 
+    const randomIndex = Math.floor(Math.random()*words.length)
+    wordDisplay.innerHTML = words[randomIndex];
+
+
+/* 
+    const randomIndex = Math.floor(Math.random() * words.length)
+    wordDisplay.innerText = words[randomIndex];
+    runNotification('success')
+ */
+
+
+
+    time = gameTime;
 }
 
 // js 2-1. input enter key (same coding with "js 2")
@@ -80,17 +99,25 @@ function buttonChange(p_text) {
 
 //js 4. axios /  new words
 
-function getWords() {
-    
 
-    axios.get('https://random-word-api.herokuapp.com/word?number=1000')
+function getWords() {    
+
+    axios.get(url)
     .then(function (response) {
         // handle success
-        console.log(response.data);
+        
+        // word.floor(response.data.random() * max);
+        // console.log(word)
+        // words.push(response.data);
 
-        const word = response.data;
-        word.floor(response.data.random() * max);
+        response.data.forEach((a) => {
+            if (a.length < 10) {
+                words.push(a)            
+            }  
+        });
 
+        console.log(words)
+        buttonChange('getWords');
 
         /*                                                                          🌊  
         🚀2. random 
@@ -105,21 +132,24 @@ function getWords() {
     })
     .then(function () {
         // always executed
-    });
-
+    });   
 }
-
 
 
 
 /* js 10. button_stop 
 
 1. change button to game start
-2.  classlist remove */
+2.  classlist remove of button
+3. reset time
+4. reset new word                                                          🌊  
+
+*/
 
 buttonStop.addEventListener('click',stop);
 
 function stop() {
     buttonChange('Game Start')
     button.classList.remove('loading');
+    time = gameTime;
 }
